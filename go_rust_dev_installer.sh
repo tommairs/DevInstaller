54a879343712c925df4e5ec1ebcb35e93cef214a
Tom Mairs <tmairs@mairs.ca>
2022-12-03 23:52:33 -0700

    Update go_rust_dev_installer.sh

--- go_rust_dev_installer.sh
+++ go_rust_dev_installer.sh
@@ -8,7 +8,7 @@ wget https://go.dev/dl/go1.19.3.linux-amd64.tar.gz
 sudo rm -rf /usr/local/go
 sudo tar -C /usr/local -xzf go1.19.3.linux-amd64.tar.gz
 export PATH=$PATH:/usr/local/go/bin
-echo "export PATH=$PATH:/usr/local/go/bin" >>  /etc/profile
+echo "export PATH=$PATH:/usr/local/go/bin" >>  ~/.profile
 go version
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
